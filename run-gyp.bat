7fa72053aea07c1d8577ca8ab8ad33c4bd35d188
Ben Vanik <ben.vanik@gmail.com>
2013-04-23 21:41:51 -0700

    Generate solution files for VS 2010 and 2012. From jdarpinian's pull request #7.

--- run-gyp.bat
+++ run-gyp.bat
@@ -1,6 +1,13 @@
-third_party\gyp\gyp.bat ^
+@echo off
+call third_party\gyp\gyp.bat ^
       -f msvs ^
       -G msvs_version=2010 ^
       --depth=. ^
-      --generator-output=build/npvr/ ^
+      --generator-output=build/vs2010/ ^
+      npvr.gyp
+call third_party\gyp\gyp.bat ^
+      -f msvs ^
+      -G msvs_version=2012 ^
+      --depth=. ^
+      --generator-output=build/vs2012/ ^
       npvr.gyp
